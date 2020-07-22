cb17eabe0a271dcb00f9e72584d18d2fc8021663
Alusio <aguignard37@gmail.com>
2020-07-22 01:49:03 -0700

    Add new design

--- uploader/forms.py
+++ uploader/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import HiddenInput
 
 from uploader.models import Document
 
--- uploader/views.py
+++ uploader/views.py
@@ -2,11 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render, redirect
 
+from s3uploader import settings
 from .forms import UploadFileForm
 from .models import Document
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.core.files.storage import FileSystemStorage
+import boto3
 
 
 @login_required
